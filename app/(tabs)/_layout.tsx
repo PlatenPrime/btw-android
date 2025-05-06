43c5a6acd405fdc4954e54a4087ed28506b8f6d5
Aleksandr Marchenko <77555146+PlatenPrime@users.noreply.github.com>
2025-05-06 07:37:20 +0300

    sklad tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { FontAwesome6 } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -33,11 +34,19 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+      
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+       <Tabs.Screen
+        name="sklad"
+        options={{
+          title: 'Склад',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="warehouse" size={24} color={color} /> ,
         }}
       />
     </Tabs>
